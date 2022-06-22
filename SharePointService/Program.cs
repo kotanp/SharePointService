714c2b5e1c41e98d45dcf02038674cfd7711499c
darvas <Amaru23_WEST>
2022-06-22 18:41:24 +0200

    Set the request timeout to 10 min

--- SharePointService/Program.cs
+++ SharePointService/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SharePointService
 {
@@ -20,7 +14,9 @@ namespace SharePointService
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseKestrel(o => { o.Limits.KeepAliveTimeout = System.TimeSpan.FromMinutes(10); })
+                    .Build();
                 });
     }
 }
